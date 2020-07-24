4e60f1c1045f0ea6fd9c524f6cff32f7ab005674
James Finley <james.finley@asynchrony.com>
2020-07-24 08:46:26 -0500

    Wasn't using auto-subscribe in RootNoteControl

--- src/components/RootNoteControl.svelte
+++ src/components/RootNoteControl.svelte
@@ -3,11 +3,6 @@
 	import { createEventDispatcher } from 'svelte';
 	import { rootNote } from '../stores.js';
 	
-	let rootNoteValue;
-	rootNote.subscribe(value => {
-		rootNoteValue = value;
-	});
-	
 	const rootClass = "root-note-control--root";
 	const accidentalClass = "root-note-control--accidental";
 	const selectedClass = 'root-note-control--selected';
@@ -58,62 +53,62 @@
 
 <nav class="root-note-control">
 	<button
-		class={rootNoteIsSelected(rootNoteValue, Notes.c, Notes.cSharp)}
+		class={rootNoteIsSelected($rootNote, Notes.c, Notes.cSharp)}
 		on:click={() => rootNote.update(value => Notes.c)}>
 		C
 	</button>
 	<button
-		class={accidentalNoteIsSelected(rootNoteValue, Notes.cSharp)}
+		class={accidentalNoteIsSelected($rootNote, Notes.cSharp)}
 		on:click={() => rootNote.update(value => Notes.cSharp)}>
 		#
 	</button>
 	<button
-		class={rootNoteIsSelected(rootNoteValue, Notes.d, Notes.dSharp)}
+		class={rootNoteIsSelected($rootNote, Notes.d, Notes.dSharp)}
 		on:click={() => rootNote.update(value => Notes.d)}>
 		D
 	</button>
 	<button
-		class={accidentalNoteIsSelected(rootNoteValue, Notes.dSharp)}
+		class={accidentalNoteIsSelected($rootNote, Notes.dSharp)}
 		on:click={() => rootNote.update(value => Notes.dSharp)}>
 		#
 	</button>
 	<button
-		class={rootNoteIsSelected(rootNoteValue, Notes.e)}
+		class={rootNoteIsSelected($rootNote, Notes.e)}
 		on:click={() => rootNote.update(value => Notes.e)}>
 		E
 	</button>
 	<button
-		class={rootNoteIsSelected(rootNoteValue, Notes.f, Notes.fSharp)}
+		class={rootNoteIsSelected($rootNote, Notes.f, Notes.fSharp)}
 		on:click={() => rootNote.update(value => Notes.f)}>
 		F
 	</button>
 	<button
-		class={accidentalNoteIsSelected(rootNoteValue, Notes.fSharp)}
+		class={accidentalNoteIsSelected($rootNote, Notes.fSharp)}
 		on:click={() => rootNote.update(value => Notes.fSharp)}>
 		#
 	</button>
 	<button
-		class={rootNoteIsSelected(rootNoteValue, Notes.g, Notes.gSharp)}
+		class={rootNoteIsSelected($rootNote, Notes.g, Notes.gSharp)}
 		on:click={() => rootNote.update(value => Notes.g)}>
 		G
 	</button>
 	<button
-		class={accidentalNoteIsSelected(rootNoteValue, Notes.gSharp)}
+		class={accidentalNoteIsSelected($rootNote, Notes.gSharp)}
 		on:click={() => rootNote.update(value => Notes.gSharp)}>
 		#
 	</button>
 	<button
-		class={rootNoteIsSelected(rootNoteValue, Notes.a, Notes.aSharp)}
+		class={rootNoteIsSelected($rootNote, Notes.a, Notes.aSharp)}
 		on:click={() => rootNote.update(value => Notes.a)}>
 		A
 	</button>
 	<button
-		class={accidentalNoteIsSelected(rootNoteValue, Notes.aSharp)}
+		class={accidentalNoteIsSelected($rootNote, Notes.aSharp)}
 		on:click={() => rootNote.update(value => Notes.aSharp)}>
 		#
 	</button>
 	<button
-		class={rootNoteIsSelected(rootNoteValue, Notes.b)}
+		class={rootNoteIsSelected($rootNote, Notes.b)}
 		on:click={() => rootNote.update(value => Notes.b)}>
 		B
 	</button>
