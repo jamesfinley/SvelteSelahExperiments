0cc88cf8c5110c4a7f6e2025b68dcd8fdc6420c8
James Finley <james.finley@asynchrony.com>
2020-07-27 14:50:06 -0500

    Added base tests of Tuning

---
+++ src/models/Tuning.test.js
@@ -0,0 +1,74 @@
+import Tuning from './Tuning.js';
+import NoteOnFret from "./NoteOnFret.js";
+import Notes, { notesInOrder } from "./Note.js";
+const faker = require('faker');
+
+let name, instrument, noteOnFrets, diatonic, tuning, spy;
+
+beforeEach(() => {
+	name = faker.random.words();
+	instrument = faker.random.words();
+	noteOnFrets = [0,1,2].map(() => new NoteOnFret(faker.random.arrayElement(notesInOrder)));
+	diatonic = faker.random.boolean();
+	tuning = new Tuning(name, instrument, noteOnFrets, diatonic);
+	spy = jest.spyOn(global.console, 'error');
+});
+
+describe('Name', () => {
+	it('has name', () => {
+		expect(tuning.name).toBe(name);
+	});
+	
+	it('consoles error when name is not string', () => {
+		name = faker.random.number();
+		tuning = new Tuning(name, instrument, noteOnFrets, diatonic);
+		
+		expect(spy).toHaveBeenCalled();
+	});
+});
+
+describe('Instrument', () => {
+	it('has instrument', () => {
+		expect(tuning.instrument).toBe(instrument);
+	});
+	
+	it('consoles error when instrument is not string', () => {
+		instrument = faker.random.number();
+		tuning = new Tuning(name, instrument, noteOnFrets, diatonic);
+		
+		expect(spy).toHaveBeenCalled();
+	});
+});
+
+describe('Note On Frets', () => {
+	it('has noteOnFrets', () => {
+		expect(tuning.noteOnFrets).toBe(noteOnFrets);
+	});
+	
+	it('consoles error when noteOnFrets is not an array', () => {
+		noteOnFrets = faker.random.words();
+		tuning = new Tuning(name, instrument, noteOnFrets, diatonic);
+		
+		expect(spy).toHaveBeenCalled();
+	});
+	
+	it('consoles error when noteOnFrets is not an array of NoteOnFret', () => {
+		noteOnFrets = [1,2,3].map(() => faker.random.words());
+		tuning = new Tuning(name, instrument, noteOnFrets, diatonic);
+		
+		expect(spy).toHaveBeenCalled();
+	});
+});
+
+describe('Diatonic', () => {
+	it('has diatonic', () => {
+		expect(tuning.diatonic).toBe(diatonic);
+	});
+	
+	it('consoles error when diatonic is not boolean', () => {
+		diatonic = faker.random.number();
+		tuning = new Tuning(name, instrument, noteOnFrets, diatonic);
+		
+		expect(spy).toHaveBeenCalled();
+	});
+});
