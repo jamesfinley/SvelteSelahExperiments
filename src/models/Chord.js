0977d9671367b89159f6d2dba8795c19fae1c4be
James Finley <james.finley@asynchrony.com>
2020-07-31 08:57:10 -0500

    Added Chord model

---
+++ src/models/Chord.js
@@ -0,0 +1,23 @@
+import { Note } from './Note.js';
+import NoteOnFret from './NoteOnFret.js';
+
+export default class Chord {
+	constructor(rootNote, type, notes) {
+		if (!rootNote || rootNote.constructor != Note) {
+			console.error("Chord: rootNote must be Note");
+			return;
+		}
+		if (!type || type.constructor != String) {
+			console.error("Chord: type must be String");
+			return;
+		}
+		if (!Array.isArray(notes) || !notes.every(note => note.constructor == NoteOnFret)) {
+			console.error("Chord: notes must be array of Note");
+			return;
+		}
+		
+		this.rootNote = rootNote;
+		this.type = type;
+		this.notes = notes;
+	}
+}
---
+++ src/models/Chord.test.js
@@ -0,0 +1,70 @@
+import Chord from './Chord.js';
+import Notes, { notesInOrder } from './Note.js';
+import NoteOnFret from "./NoteOnFret.js";
+
+const faker = require('faker');
+
+let chord, rootNote, notes, type, spy;
+
+beforeEach(() => {
+	rootNote = faker.random.arrayElement(notesInOrder);
+	type = faker.random.words();
+	notes = Array.from(Array(faker.random.number({
+		'min': 1,
+		'max': 7
+	})).keys()).map(() => {
+		return new NoteOnFret(faker.random.arrayElement(notesInOrder), faker.random.number({
+			'min': 0,
+			'max': 5
+		}));
+	});
+	chord = new Chord(rootNote, type, notes);
+	
+	spy = jest.spyOn(global.console, 'error');
+})
+
+describe('Root Note', () => {
+	it('has rootNote', () => {
+		expect(chord.rootNote).toBe(rootNote);
+	});
+	
+	it('consoles error when rootNote is not Note', () => {
+		rootNote = faker.random.number();
+		chord = new Chord(rootNote, type, notes);
+		
+		expect(spy).toHaveBeenCalled();
+	});
+});
+
+describe('Type', () => {
+	it('has type', () => {
+		expect(chord.type).toBe(type);
+	});
+	
+	it('consoles error when type is not string', () => {
+		type = faker.random.number();
+		chord = new Chord(rootNote, type, notes);
+		
+		expect(spy).toHaveBeenCalled();
+	});
+});
+
+describe('Notes', () => {
+	it('has notes', () => {
+		expect(chord.notes).toBe(notes);
+	});
+	
+	it('consoles error when notes is not array', () => {
+		notes = faker.random.number();
+		chord = new Chord(rootNote, type, notes);
+		
+		expect(spy).toHaveBeenCalled();
+	});
+	
+	it('consoles error when notes is not array of NoteOnFret', () => {
+		notes = [faker.random.number(), faker.random.number(), faker.random.number()];
+		chord = new Chord(rootNote, type, notes);
+		
+		expect(spy).toHaveBeenCalled();
+	});
+});
