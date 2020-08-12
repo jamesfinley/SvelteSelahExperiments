599c1ab490dcec45a1583944ed07f1d072fa53fe
James Finley <james.finley@asynchrony.com>
2020-08-12 10:41:29 -0500

    Magic now shows real chords

--- src/components/screens/Magic/Magic.svelte
+++ src/components/screens/Magic/Magic.svelte
@@ -2,15 +2,26 @@
 	import { rootNote, scale, tuning } from '../../../stores.js';
 	
 	import Chord from '../../Chord/Chord.svelte';
-	import ChordModel from '../../../models/Chord.js';
-	import ChordType from '../../../models/ChordType.js';
-	import Notes from '../../../models/Note.js';
-	import NoteOnFret from '../../../models/NoteOnFret.js';
+	import Voicings from '../../../models/Voicings.js';
 	
 	function getNumberWithOrdinal(n) {
 		const s = ["th", "st", "nd", "rd"],
 		      v = n % 100;
 		return n + (s[(v - 20) % 10] || s[v] || s[0]);
+	}
+	
+	$: chordTypesForNotes = $rootNote.chordsForScale($scale);
+	let chordsForNotes;
+	$: {
+		chordsForNotes = {};
+		$rootNote.notesForScale($scale).map(note => {
+			let results = {};
+			chordTypesForNotes[note.name].forEach(chordType => {
+				const chords = Voicings.forChordTypeWithRootNoteAndTuning(chordType, note, $tuning, $tuning.maxFrets - 1);
+				results[chordType.shortName] = chords ? chords[0] : null;
+			});
+			chordsForNotes[note.name] = results;
+		})
 	}
 </script>
 
@@ -23,64 +34,19 @@
 </style>
 
 <div class="magic">
-	{#each $rootNote.notesForScale($scale) as $note, index}
+	{#each $rootNote.notesForScale($scale) as note, index}
 		<section class="magic--degree">
-			<h2 class="magic--degree--header">Chords in {getNumberWithOrdinal(index + 1)} Degree: {$note.name}</h2>
+			<h2 class="magic--degree--header">Chords in {getNumberWithOrdinal(index + 1)} Degree: {note.name}</h2>
 			<div class="magic--degree--chords">
-				<Chord fretCount=7 chord={new ChordModel(
-				Notes.c,
-				new ChordType('major'),
-				[
-				new NoteOnFret(Notes.c, 0),
-				new NoteOnFret(Notes.g, 0),
-				new NoteOnFret(Notes.e, 2),
-				new NoteOnFret(Notes.c, 3),
-				])} />
-				<Chord fretCount=7 chord={new ChordModel(
-				Notes.c,
-				new ChordType('major'),
-				[
-				new NoteOnFret(Notes.c, 7),
-				new NoteOnFret(Notes.g, 5),
-				new NoteOnFret(Notes.e, 2),
-				new NoteOnFret(Notes.c, 3),
-				])} />
-				<Chord fretCount=7 chord={new ChordModel(
-				Notes.c,
-				new ChordType('major'),
-				[
-				new NoteOnFret(Notes.c, 0),
-				new NoteOnFret(Notes.g, 0),
-				new NoteOnFret(Notes.e, 2),
-				new NoteOnFret(Notes.c, 3),
-				])} />
-				<Chord fretCount=7 chord={new ChordModel(
-				Notes.c,
-				new ChordType('major'),
-				[
-				new NoteOnFret(Notes.c, 7),
-				new NoteOnFret(Notes.g, 5),
-				new NoteOnFret(Notes.e, 2),
-				new NoteOnFret(Notes.c, 3),
-				])} />
-				<Chord fretCount=7 chord={new ChordModel(
-				Notes.c,
-				new ChordType('major'),
-				[
-				new NoteOnFret(Notes.c, 0),
-				new NoteOnFret(Notes.g, 0),
-				new NoteOnFret(Notes.e, 2),
-				new NoteOnFret(Notes.c, 3),
-				])} />
-				<Chord fretCount=7 chord={new ChordModel(
-				Notes.c,
-				new ChordType('major'),
-				[
-				new NoteOnFret(Notes.c, 7),
-				new NoteOnFret(Notes.g, 5),
-				new NoteOnFret(Notes.e, 2),
-				new NoteOnFret(Notes.c, 3),
-				])} />
+				{#each chordTypesForNotes[note.name] as chordType}
+					{#if chordsForNotes[note.name][chordType.shortName]}
+						<Chord
+							fretCount={$tuning.maxFrets}
+							showName
+							chord={chordsForNotes[note.name][chordType.shortName]}
+							/>
+					{/if}
+				{/each}
 			</div>
 		</section>
 	{/each}
